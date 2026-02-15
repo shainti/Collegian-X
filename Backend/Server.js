453d1fdb5d6c1b672a59e31232e52cf21098c471
shainti_kashyap <shaintykashyap@gmail.com>
2026-02-15 15:12:08 +0530

    add the port

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,7 +3,7 @@ const app = require('./src/app')
 const connectdb = require("./src/db/db")
 
 connectdb();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = 'localhost'
 app.listen(PORT, HOST, ()=>{
     console.log(`server start at http://${HOST}:${PORT}`);
