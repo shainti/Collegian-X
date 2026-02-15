a072fc1d50237a46cef1a0769b406f7e55fcfa59
shainti_kashyap <shaintykashyap@gmail.com>
2026-02-16 01:31:07 +0530

    change the sercer:

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,10 +2,6 @@ require('dotenv').config();
 const app = require("./src/app");
 const connectdb = require("./src/db/db");
 
-console.log('\n=== ENVIRONMENT VARIABLES CHECK ===');
-console.log('Collegian_Email:', process.env.Collegian_Email);
-console.log('Collegian_pass:', process.env.Collegian_pass ? '✅ EXISTS' : '❌ MISSING');
-console.log('===================================\n');
 
 connectdb();
 
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:5173",
-     "https://collegian-x-fgy8.vercel.app"
+     "https://collegianx.vercel.app",
     ],
   credentials: true
 }));
