4396b732b1b97cc9d5d03e4dd9d2802308d0dca0
shainti_kashyap <shaintykashyap@gmail.com>
2026-05-12 22:19:23 +0530

    new commit

--- Backend/src/Controller/auth.controller.js
+++ Backend/src/Controller/auth.controller.js
@@ -8,7 +8,7 @@ const { sendverificationcode } = require('../middleware/Email.confiq');
 const { GetInboundEmailEvents } = require("@getbrevo/brevo");
 
 
-//add new commit 
+//add new commit new value 
 exports.registerstudents = async (req, res, next) => {
   try {
     const { FullName, email, Department, Semester, CollegeRollNo, password } =
